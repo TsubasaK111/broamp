e291d6063659f5bd5031c360aed4d75b984799f8
TsubasaK111 <Tsubasa.K111@gmail.com>
2018-08-28 22:38:05 +0900

    refactor: split mutations out

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App.vue";
 
 import { IpfsPlugin, RoomVuexPlugin } from "./rooms";
 import config from "./config";
-import './audio/Odesza-Above_The_Middle.mp3';
+import mutations from "./store/mutations";
 
 // const ipfsRoom = new IpfsPlugin(config.ipfs);
 const vuexRoom = new RoomVuexPlugin(config.ipfs);
@@ -24,45 +24,7 @@ const store = new Vuex.Store({
     audioVolume: "0.7",
     audioStatus: "created",
   },
-  mutations: {
-    ipfsConnection(state, newStatus) {
-      state.ipfsStatus = newStatus;
-    },
-    peerJoined(state, newPeer) {
-      if (state.peers.includes(newPeer)) return;
-      state.peers.push(newPeer);
-    },
-    peerLeft(state, leftPeer) {
-      state.peers = state.peers.filter(peer => peer !== leftPeer);
-    },
-    addMessage(state, newMessage) {
-      state.messages.push(newMessage);
-    },
-    updateAudioSrc(state, newAudioSrc) {
-      state.audioSrc = newAudioSrc;
-    },
-    updateAudioStatus(state, newAudioStatus) {
-      state.audioStatus = newAudioStatus;
-    },
-    audioPlay(state){
-      state.audioPaused = false;
-    },
-    audioPause(state){
-      state.audioPaused = true;
-    },
-    broadcastChange(state, newAudioSrc){
-      console.log(`broadcastChange is trigged`)
-      state.audioSrc = newAudioSrc;
-    },
-    broadcastPlay(state) {
-      if (state.audioStatus !== 'canPlayThrough') throw Error('audio is not ready to play through, wait!')
-
-      state.audioPaused = false;
-    },
-    broadcastPause(state) {
-      state.audioPaused = true;
-    },
-  },
+  mutations: mutations,
   plugins: [vuexRoom]
 });
 
---
+++ src/store/mutations.js
@@ -0,0 +1,40 @@
+
+export const mutations = {
+  ipfsConnection(state, newStatus) {
+    state.ipfsStatus = newStatus;
+  },
+  peerJoined(state, newPeer) {
+    if (state.peers.includes(newPeer)) return;
+    state.peers.push(newPeer);
+  },
+  peerLeft(state, leftPeer) {
+    state.peers = state.peers.filter(peer => peer !== leftPeer);
+  },
+  addMessage(state, newMessage) {
+    state.messages.push(newMessage);
+  },
+  updateAudioSrc(state, newAudioSrc) {
+    state.audioSrc = newAudioSrc;
+  },
+  updateAudioStatus(state, newAudioStatus) {
+    state.audioStatus = newAudioStatus;
+  },
+  audioPlay(state){
+    state.audioPaused = false;
+  },
+  audioPause(state){
+    state.audioPaused = true;
+  },
+  broadcastChange(state, newAudioSrc){
+    console.log(`broadcastChange is trigged`)
+    state.audioSrc = newAudioSrc;
+  },
+  broadcastPlay(state) {
+    if (state.audioStatus !== 'canPlayThrough') throw Error('audio is not ready to play through, wait!')
+
+    state.audioPaused = false;
+  },
+  broadcastPause(state) {
+    state.audioPaused = true;
+  },
+}
