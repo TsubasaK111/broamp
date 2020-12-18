eb66ca1543d9c921703924c4bf99ce56959f3ffb
Tsubasa Kondo <tsubasa.k111@gmail.com>
2020-12-19 08:06:46 +0900

    chore: add more rendezvous servers

--- ipfs.config.js
+++ ipfs.config.js
@@ -15,6 +15,12 @@ const config = {
           "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
           "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
           
+          // experimental webrtc
+          '/ip4/198.46.197.197/tcp/4001/ipfs/QmdXiwDtfKsfnZ6RwEcovoWsdpyEybmmRpVDXmpm5cpk2s',
+
+          // IPFS dev rendezvous server
+          // '/dns4/star-signal.cloud.ipfs.team/wss/p2p-webrtc-star',
+
           // locally hosted websocket star
           // "/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star",
           // local brave browser hosted star
@@ -22,6 +28,7 @@ const config = {
 
           // config default
           // "/ip4/0.0.0.0/tcp/4002",
+          // another config default, however creating ws server in browser is not allowed
           // "/ip4/127.0.0.1/tcp/4003/ws",
         ],
       },
